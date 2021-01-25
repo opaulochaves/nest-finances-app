2f54ffd06d248616601e7b07910f172c749686bf
Paulo Chaves <opaulochaves@gmail.com>
2021-01-25 11:12:37 -0300

    Initial commit

---
+++ src/modules/main/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from './../config';
+import { AuthModule } from './../auth';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [__dirname + './../**/**.entity{.ts,.js}'],
+          synchronize: configService.get('DB_SYNC') === 'true',
+        } as TypeOrmModuleAsyncOptions;
+      },
+    }),
+    ConfigModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
